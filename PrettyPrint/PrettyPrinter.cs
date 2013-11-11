3b1981ab77154a03d9afe4e0fc42416b6253f3d8
Adam Kosinski <still.adam.k@gmail.com>
2013-11-12 00:15:21 +0100

    Basic print functions

---
+++ PrettyPrint/PrettyPrinter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrettyPrint
+{
+    public class PrettyPrinter
+    {
+        public string Print(object something)
+        {
+            var className = something.GetType().Name;
+
+            var properties = something.GetType().GetProperties();
+
+            var output = new StringBuilder();
+            output.AppendLine(className + ':');
+
+            foreach (var property in properties)
+            {
+                if (property.PropertyType != typeof(string) && property.GetValue(something) is IEnumerable)
+                {
+                    output.AppendLine(property.Name + "[]:" + CountEnumerable(property.GetValue(something) as IEnumerable));    
+                }
+                else
+                {
+                    output.AppendLine(property.Name + ':' + property.GetValue(something));        
+                }
+            }
+
+            return output.ToString();
+        }
+
+        private int CountEnumerable(IEnumerable e)
+        {
+            int count = 0;
+
+            var enumerator = e.GetEnumerator();
+            while (enumerator.MoveNext() == true)
+            {
+                count ++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ PrettyPrintTests/PrinterTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using PrettyPrint;
+
+namespace PrettyPrintTests
+{
+    public class PrinterTests
+    {
+        class TestObject
+        {
+            public string SomeProperty { get; set; }
+            public string NextProperty { get; set; }
+
+            public string[] SomeCollection { get; set; }
+
+        }
+
+        [Fact]
+        public void Print__prints_objects_class_name()
+        {
+            var subject = new PrettyPrinter();
+            var something = new TestObject();
+
+            string result = subject.Print(something);
+            result.Should().StartWith("TestObject");
+            
+            Console.WriteLine(result);
+        }
+
+        
+        [Fact]
+        public void Print__prints_object_properties_with_value()
+        {
+            var subject = new PrettyPrinter();
+            var something = new TestObject();
+            something.SomeProperty = "Property Content";
+
+            var s = subject.Print(something);
+
+            Console.WriteLine(s);
+            s.Should().Contain("SomeProperty:Property Content");
+        }
+
+        [Fact]
+        public void Print__prints_all_properties_with_values()
+        {
+            var subject = new PrettyPrinter();
+            var something = new TestObject();
+            something.SomeProperty = "Property Content";
+            something.NextProperty = "Next Content";
+
+            var s = subject.Print(something);
+
+            Console.WriteLine(s);
+            s.Should().Contain("SomeProperty:Property Content");
+            s.Should().Contain("NextProperty:Next Content");
+        }
+
+        [Fact]
+        public void Print__property_is_a_collection_writes_collection_count()
+        {
+            var subject = new PrettyPrinter();
+            var something = new TestObject();
+
+            something.SomeCollection = new[] {""};
+
+            var s = subject.Print(something);
+
+            Console.WriteLine(s);
+            s.Should().Contain("SomeCollection[]:1");
+        }
+
+    }
+}
